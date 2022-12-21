1298e552d89115c133c87d31d7e76f5121b69714
guspnet <guspnet@gmail.com>
2022-12-21 10:15:07 -0300

    Fim da aula04

--- alura-books/src/App.js
+++ alura-books/src/App.js
@@ -1,5 +1,7 @@
 import Header from './componentes/Header'
+import Pesquisa from './componentes/Pesquisa'
 import styled from 'styled-components'
+
 
 const AppContainer = styled.div`
     width: 100vw;
@@ -11,6 +13,7 @@ function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa />
     </AppContainer>
   );
 }
